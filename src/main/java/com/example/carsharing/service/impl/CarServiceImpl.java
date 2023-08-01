8ca7adedaa295fa288f0f32066e815d09c934781
Viktor Bykh <bykhviktor21@gmail.com>
2023-08-01 12:38:06 +0300

    added CarService and UserService

--- src/main/java/com/example/carsharing/service/impl/CarServiceImpl.java
+++ src/main/java/com/example/carsharing/service/impl/CarServiceImpl.java
@@ -1,12 +1,51 @@
 package com.example.carsharing.service.impl;
 
+import com.example.carsharing.model.Car;
 import com.example.carsharing.repository.CarRepository;
 import com.example.carsharing.service.CarService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CarServiceImpl implements CarService {
     private final CarRepository carRepository;
+
+
+    @Override
+    public Car add(Car car) {
+        return carRepository.save(car);
+    }
+
+    @Override
+    public List<Car> getAll() {
+        return (List<Car>) carRepository.findAll();
+    }
+
+    @Override
+    public Car getById(Long id) {
+        return carRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Not found car with id: " + id));
+    }
+
+    @Override
+    @Transactional
+    public Car update(Long id, Car car) {
+        Car carFromDb = carRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Not found car with id: " + id));
+        carFromDb.setId(car.getId());
+        carFromDb.setModel(car.getModel());
+        carFromDb.setBrand(car.getBrand());
+        carFromDb.setType(car.getType());
+        carFromDb.setInventory(car.getInventory());
+        carFromDb.setDailyFee(car.getDailyFee());
+        return carFromDb;
+    }
+
+    @Override
+    public void delete(Long id) {
+        carRepository.deleteById(id);
+    }
 }
--- src/main/java/com/example/carsharing/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/carsharing/service/impl/UserServiceImpl.java
@@ -1,12 +1,45 @@
 package com.example.carsharing.service.impl;
 
+import com.example.carsharing.model.User;
 import com.example.carsharing.repository.UserRepository;
 import com.example.carsharing.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public User updateUserRole(Long id, User.Role role) {
+        User userFromDb = userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Not found user with id: " + id)
+        );
+        userFromDb.setRole(role);
+        return userFromDb;
+    }
+
+    @Override
+    public User getProfileInfo(User user) {
+        return userRepository.findById(user.getId()).orElseThrow(
+                () -> new RuntimeException("Not found profile info for user: " + user)
+        );
+    }
+
+    @Override
+    @Transactional
+    public User updateProfileInfo(User user) {
+        User userFromDb = userRepository.findById(user.getId()).orElseThrow(
+                () -> new RuntimeException("Not found profile info for user: " + user));
+        userFromDb.setId(user.getId());
+        userFromDb.setEmail(user.getEmail());
+        userFromDb.setFirstName(user.getFirstName());
+        userFromDb.setLastName(user.getLastName());
+        userFromDb.setPassword(user.getPassword());
+        userFromDb.setRole(user.getRole());
+        return userFromDb;
+    }
 }
