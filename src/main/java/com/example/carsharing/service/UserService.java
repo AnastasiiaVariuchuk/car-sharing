7988e92e9d71dd848bf955eed63bc07dd55d3d59
Viktor Bykh <129959009+ViktorBykh@users.noreply.github.com>
2023-08-01 12:42:56 +0300

    Added CarService and UserService

--- src/main/java/com/example/carsharing/service/UserService.java
+++ src/main/java/com/example/carsharing/service/UserService.java
@@ -1,4 +1,11 @@
 package com.example.carsharing.service;
 
+import com.example.carsharing.model.User;
+
 public interface UserService {
+    User updateUserRole(Long id, User.Role role);
+
+    User getProfileInfo(User user);
+
+    User updateProfileInfo(User user);
 }
