88268106ca91a64fb6f2ee5c7445b29c0784e7f9
Petro Kobevko <kobevkopi@gmail.com>
2023-08-01 17:45:55 +0200

    Added RentalService realization (#10)
    * Added RentalRepository realization
    
    * Refactored rental service
    
    * Refactored rental service and sql table

--- src/main/java/com/example/carsharing/repository/RentalRepository.java
+++ src/main/java/com/example/carsharing/repository/RentalRepository.java
@@ -1,9 +1,15 @@
 package com.example.carsharing.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import com.example.carsharing.model.Rental;
+import com.example.carsharing.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RentalRepository extends CrudRepository<Rental, Long> {
+    List<Rental> findRentalByUserAndActualReturnDateIsNull(User user);
+
+    List<Rental> findRentalByUserAndActualReturnDateIsNotNull(User user);
 }
---
+++ src/main/java/com/example/carsharing/service/impl/RentalServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.carsharing.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import com.example.carsharing.model.Car;
+import com.example.carsharing.model.Rental;
+import com.example.carsharing.model.User;
+import com.example.carsharing.repository.RentalRepository;
+import com.example.carsharing.service.CarService;
+import com.example.carsharing.service.RentalService;
+import com.example.carsharing.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RentalServiceImpl implements RentalService {
+    private final int MIN_AMOUNT_TO_RENT = 1;
+    private final RentalRepository rentalRepository;
+    private final CarService carService;
+    private final UserService userService;
+
+    @Override
+    public Rental add(Long carId, Long userId, LocalDateTime returnDate) {
+        Car car = carService.getById(carId);
+        User user = userService.getById(userId);
+        if (car.getInventory() < MIN_AMOUNT_TO_RENT) {
+            throw new RuntimeException("Can't rent car with id: " + carId);
+        }
+        Rental rental = new Rental();
+        rental.setRentalDate(LocalDateTime.now());
+        rental.setReturnDate(returnDate);
+        rental.setCar(car);
+        rental.setUser(user);
+        return rentalRepository.save(rental);
+    }
+
+    @Override
+    public List<Rental> getByUserAndActiveness(User user, boolean isActive) {
+        return isActive ? rentalRepository.findRentalByUserAndActualReturnDateIsNull(user) :
+                rentalRepository.findRentalByUserAndActualReturnDateIsNotNull(user);
+    }
+
+    @Override
+    public Rental getById(Long id) {
+        return rentalRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("Can't get rental with id: " + id));
+    }
+
+    @Override
+    public Rental terminate(Long id) {
+        Rental rental = getById(id);
+        if (rental.getActualReturnDate() != null) {
+            throw new RuntimeException("Rental with id " + id + " already terminated");
+        }
+        rental.setActualReturnDate(LocalDateTime.now());
+        rentalRepository.save(rental);
+        Car car = rental.getCar();
+        car.setInventory(car.getInventory() + 1);
+        carService.update(car.getId(), car);
+        return rental;
+    }
+}
