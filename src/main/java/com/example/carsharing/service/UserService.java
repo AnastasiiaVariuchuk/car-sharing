3f0f79e31b09f7baf0dfe085144623d5064217c2
Viktor Bykh <bykhviktor21@gmail.com>
2023-08-01 19:49:14 +0300

    added SwaggerConfig and fixed some issues

--- src/main/java/com/example/carsharing/service/UserService.java
+++ src/main/java/com/example/carsharing/service/UserService.java
@@ -3,6 +3,8 @@ package com.example.carsharing.service;
 import com.example.carsharing.model.User;
 
 public interface UserService {
+    User add(User user);
+
     User updateUserRole(Long id, User.Role role);
 
     User getProfileInfo(User user);
--- src/main/java/com/example/carsharing/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/carsharing/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.NoSuchElementException;
 import com.example.carsharing.model.User;
 import com.example.carsharing.repository.UserRepository;
 import com.example.carsharing.service.UserService;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +15,15 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
     @Override
+    public User add(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
     @Transactional
     public User updateUserRole(Long id, User.Role role) {
         User userFromDb = userRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Not found user with id: " + id)
+                () -> new NoSuchElementException("Not found user with id: " + id)
         );
         userFromDb.setRole(role);
         return userFromDb;
@@ -26,7 +32,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getProfileInfo(User user) {
         return userRepository.findById(user.getId()).orElseThrow(
-                () -> new RuntimeException("Not found profile info for user: " + user)
+                () -> new NoSuchElementException("Not found profile info for user: " + user)
         );
     }
 
@@ -34,7 +40,7 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public User updateProfileInfo(User user) {
         User userFromDb = userRepository.findById(user.getId()).orElseThrow(
-                () -> new RuntimeException("Not found profile info for user: " + user));
+                () -> new NoSuchElementException("Not found profile info for user: " + user));
         userFromDb.setId(user.getId());
         userFromDb.setEmail(user.getEmail());
         userFromDb.setFirstName(user.getFirstName());
