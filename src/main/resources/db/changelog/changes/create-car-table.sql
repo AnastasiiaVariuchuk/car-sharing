fcf070b58382588a31136a8b91684dd1b35e5f75
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-08-01 13:47:16 +0300

    added the required restrictions

--- src/main/resources/db/changelog/changes/create-car-table.sql
+++ src/main/resources/db/changelog/changes/create-car-table.sql
@@ -1,12 +1,13 @@
 CREATE TABLE IF NOT EXISTS `cars`
 (
     `id`        bigint NOT NULL AUTO_INCREMENT,
-    `model`     VARCHAR(255) DEFAULT NULL,
-    `brand`     VARCHAR(255) DEFAULT NULL,
+    `model`     VARCHAR(255) NOT NULL DEFAULT 'UNKNOWN_MODEL',
+    `brand`     VARCHAR(255) NOT NULL DEFAULT 'UNKNOWN_BRAND',
     `type`      VARCHAR(255) DEFAULT NULL,
-    `inventory` INT          DEFAULT NULL,
-    `daily_fee` DECIMAL      DEFAULT NULL,
+    `inventory` INT          NOT NULL DEFAULT '0',
+    `daily_fee` DECIMAL      NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
+
