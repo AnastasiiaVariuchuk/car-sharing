6ddb30d689d70b9e75c67fa674f4a4d80b4aea3b
Stanislav Zlishchev <110261412+sZlishchev@users.noreply.github.com>
2023-08-02 22:56:07 +0300

    Feature/impoving payment (#22)
    * implemented paymentController and paymentProvider
    
    * fixed PaymentController, PaymentService and added PaymentRepository
    
    * fixed checkstyle
    
    * fixed PaymentController and added PaymentRepository
    
    * added PaymentResponseDto and succesful link implementation

--- src/main/java/com/example/carsharing/controller/PaymentController.java
+++ src/main/java/com/example/carsharing/controller/PaymentController.java
@@ -1,55 +1,61 @@
 package com.example.carsharing.controller;
 
-import com.example.carsharing.dto.request.RentalRequestDto;
-import com.example.carsharing.mapper.RentalMapper;
+import com.example.carsharing.dto.response.PaymentResponseDto;
 import com.example.carsharing.model.Payment;
 import com.example.carsharing.model.Rental;
 import com.example.carsharing.payment.PaymentProvider;
 import com.example.carsharing.service.PaymentService;
+import com.example.carsharing.service.RentalService;
+import com.example.carsharing.service.mapper.PaymentMapper;
 import com.example.carsharing.strategy.TotalAmountHandlerStrategy;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("payments/")
+@RequestMapping("/payments")
 public class PaymentController {
     private PaymentProvider paymentProvider;
-    private RentalMapper rentalMapper;
+    private RentalService rentalService;
     private PaymentService paymentService;
     private TotalAmountHandlerStrategy strategy;
+    private PaymentMapper paymentMapper;
 
-    @GetMapping("success/")
-    public String successPayment() {
+    @GetMapping("/success")
+    public String successPayment(@RequestParam("session_id") String sessionId) {
+        if (paymentProvider.isSessionPaid(sessionId)) {
+            paymentService.setPaid(sessionId);
+        }
         return "Successful payment";
     }
 
-    @GetMapping("cancel/")
+    @GetMapping("/cancel")
     public String cancelPayment() {
         return "Something went wrong";
     }
 
     @PostMapping
-    public Session createSession(@RequestBody RentalRequestDto rentalRequestDto)
+    public PaymentResponseDto createSession(@RequestParam Long rentalId)
             throws StripeException {
-        Rental rental = rentalMapper.mapToEntity(rentalRequestDto);
+        Rental rental = rentalService.getById(rentalId);
         Payment payment = new Payment();
         payment.setId(rental.getId());
+        payment.setRental(rental);
         payment.setStatus(Payment.Status.PENDING);
         payment.setType(paymentService.findType(rental));
         payment.setAmountToPay(strategy.getToTalAmountHandler(payment.getType())
                 .getTotalAmount(rental));
-        Session session = paymentProvider.createSession(payment);
+        Session session = paymentProvider.createSession(payment, rental.getCar());
         payment.setSessionId(session.getId());
         payment.setSessionUrl(session.getUrl());
-        // TODO: create payment service and dao and save payment
-        return session; //TODO: crate SessionResponseDto
+        paymentService.add(payment);
+        return paymentMapper.mapToDto(payment);
     }
 
 }
