d0bf47208c1826de96faf55225b2acfe2c0ba74c
Stas Zlishchev <zlishchev@gmail.com>
2023-08-03 20:43:46 +0300

    cover strategy with tests and fix strategy logic

--- src/test/java/com/example/carsharing/strategy/impl/FineTotalAmountHandlerTest.java
+++ src/test/java/com/example/carsharing/strategy/impl/FineTotalAmountHandlerTest.java
@@ -1,9 +1,65 @@
 package com.example.carsharing.strategy.impl;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(MockitoExtension.class)
+import com.example.carsharing.model.Rental;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class FineTotalAmountHandlerTest {
+    private FineTotalAmountHandler fineHandler;
 
+    @BeforeEach
+    void setUp() {
+        fineHandler = new FineTotalAmountHandler();
+    }
+
+    @Test
+    void getOverdueDays_zeroOverdueDays_ok() {
+        Rental rental = new Rental();
+        rental.setRentalDate(LocalDateTime.of(2023, 8,3,10,30));
+        rental.setReturnDate(LocalDateTime.of(2023, 8,3,15,30));
+        rental.setActualReturnDate(LocalDateTime.of(2023, 8,3,15,30));
+        BigDecimal actual = fineHandler.getTotalAmount(rental, BigDecimal.ONE);
+        BigDecimal expected = BigDecimal.ONE;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getOverdueDays_oneOverdueDay_ok() {
+        Rental rental = new Rental();
+        rental.setRentalDate(LocalDateTime.of(2023, 8,3,10,30));
+        rental.setReturnDate(LocalDateTime.of(2023, 8,3,15,30));
+        rental.setActualReturnDate(LocalDateTime.of(2023, 8,4,15,30));
+        BigDecimal actual = fineHandler.getTotalAmount(rental, BigDecimal.ONE);
+        BigDecimal expected = BigDecimal.valueOf(2.25);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getOverdueDays_fiveOverdueDays_ok() {
+        Rental rental = new Rental();
+        rental.setRentalDate(LocalDateTime.of(2023, 8,3,10,30));
+        rental.setReturnDate(LocalDateTime.of(2023, 8,3,15,30));
+        rental.setActualReturnDate(LocalDateTime.of(2023, 8,8,15,30));
+        BigDecimal actual = fineHandler.getTotalAmount(rental, BigDecimal.valueOf(5L));
+        BigDecimal expected = BigDecimal.valueOf(36.25);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getOverdueDays_3OverdueDays_ok() {
+        Rental rental = new Rental();
+        rental.setRentalDate(LocalDateTime.of(2023, 8,3,10,30));
+        rental.setReturnDate(LocalDateTime.of(2023, 8,3,15,30));
+        rental.setActualReturnDate(LocalDateTime.of(2023, 8,13,15,30));
+        BigDecimal actual = fineHandler.getTotalAmount(rental, BigDecimal.valueOf(7L));
+        DecimalFormat f = new DecimalFormat("##.00");
+        BigDecimal value = BigDecimal.valueOf(94.5);
+        BigDecimal expected = value.setScale(2, BigDecimal.ROUND_HALF_UP);
+        assertEquals(expected, actual);
+    }
 }
