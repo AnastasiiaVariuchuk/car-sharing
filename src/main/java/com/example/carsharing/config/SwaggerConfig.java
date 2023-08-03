7172c3e16eee5be9a589e4514b04d2031c3efdf7
Mykola Panasiuk <66474575+m-pnsk@users.noreply.github.com>
2023-08-03 10:00:09 +0300

    added authorization to swagger (#25)
    * added authorization to swagger
    
    * fixed checkstyle

--- src/main/java/com/example/carsharing/config/SwaggerConfig.java
+++ src/main/java/com/example/carsharing/config/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package com.example.carsharing.config;
 
+import java.util.Collections;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,9 +32,26 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(this.apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.carsharing.controller"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        return Collections.singletonList(new SecurityReference("JWT",
+                new AuthorizationScope[]{
+                        new AuthorizationScope("global", "accessEverything")
+                }));
     }
 }
