9f4af4040e63ec4086f9cdd21ee8319e55a7a295
Vitalii Parkhomenko <96497090+viprko@users.noreply.github.com>
2023-08-04 10:00:39 +0300

    Mini fixes (#57)
    * Another mini fixes after manual testing
    
    * Fixed query for PaymentRepository
    
    * Additional refactoring
    
    * Additional refactoring
    
    * Additional refactoring #2

--- src/main/java/com/example/carsharing/config/SecurityConfig.java
+++ src/main/java/com/example/carsharing/config/SecurityConfig.java
@@ -44,13 +44,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/register", "/inject", "/login", "/health", "/v2/api-docs",
                         "/swagger-resources/**", "/swagger-ui.html",
-                        "/webjars/**", "/payments/**","/cars").permitAll()
+                        "/webjars/**", "/payments/success",
+                        "/payments/cancel", "/cars").permitAll()
                 .antMatchers(HttpMethod.POST, "/cars").hasRole(User.Role.MANAGER.name())
                 .antMatchers("/cars/*").hasRole(User.Role.MANAGER.name())
                 .antMatchers(HttpMethod.DELETE, "/cars/*", "/users/*")
                 .hasRole(User.Role.MANAGER.name())
+                .antMatchers(HttpMethod.GET, "/payments").hasRole(User.Role.MANAGER.name())
+                .antMatchers(HttpMethod.GET, "/payments/my").hasRole(User.Role.CUSTOMER.name())
                 .antMatchers(HttpMethod.PUT, "/users/*/role").hasRole(User.Role.MANAGER.name())
                 .antMatchers(HttpMethod.PATCH, "/cars/*").hasRole(User.Role.MANAGER.name())
+                .antMatchers(HttpMethod.GET, "/rentals/users/*", "rentals/*"
+        ).hasRole(User.Role.MANAGER.name())
                 .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
